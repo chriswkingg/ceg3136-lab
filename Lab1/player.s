23a668e8557ce8cc47340e75e279a76c915bfd73
Chriswkingg <chriswkingg@gmail.com>
2023-09-28 20:04:50 -0400

    Working lab1

--- Lab1/player.s
+++ Lab1/player.s
@@ -9,54 +9,62 @@
 update_points_s FUNCTION	; char update_points_s(player_t *player, uint8_t target_info);
 	; r0 *player
 	; r1 target_info	
-	MOV r2, r0					; move player pointer to r2
-	MOV r3, #0x00				; points_urned = 0
-	
-    LDRH r0,[r2,#0x00]	;load value of nAttempts
-	ADDS r0,r0,#1		; increment
-    STRH r0,[r2,#0x00]	; store nAttempts
-	
-	AND  r0,r1,#0xFC	;
-	CBZ  r0, endandreturn	;
-	
-	AND r0, r1, #0x02
-	CBNZ r0, endandreturn	;
-	
-	LDRH r0,[r2,#0x02]	;load value of nHits
-	ADDS r0,r0,#1		; increment
-    STRH r0,[r2,#0x02]	; store nHits
-	
-	
-	UBFX r0, r1, #0x02, #0x03 ; type is in r0
-	
-	UBFX r4, r1, #0x05, #0x02 ;indx is in r4
-	
-    SUBS r5,r0,#1			; type - 1 in r5
-	ADDS r6,r2,#6			; player pieces hit offset
-	ADD r5,r6,r5,LSL #2		; pieces hit + type-1 left shifted 2
-	LDRB r5,[r5,r4]			; save pieces hit value to r5
-	ADDS r5,r5,#1			; increment
-	UXTB r7,r5				; removes zeros???
-	SUBS r5,r0,#1			; subtract 1 from incremented pieces hit value
-	ADD r5,r6,r5,LSL #2		; recalculate pieces hit address
-	STRB r7,[r5,r4]			; store new value at same address
-	
-	ADDS r3,r3, #0x01		; increment points_urned
-	
-	SUBS r5, r0, #0x01		;
-	ADD r5, r6, r5, LSL #0x02;
-	LDRB r5, [r5,r4]		; load pieces_hit value into r5
-	CMP r5, r0				; compare pieces_hit to type
-	BNE endandreturn
-	
-	ADD r3, r3, r0			; points_urned += type
-	
-endandreturn	LDRH r0, [r2,#0x04]		;load value of points
-	ADD r0, r0, r3			; add points_urned to player.points
-	STRH r0, [r2,#0x04]		; save it
-	
-	MOV r0, r3				; move points_urned to return value 
-	BX	LR						; return 0
+		PUSH          {r4-r7,lr};
+		MOV           r2,r0;
+
+		MOVS          r3,#0x00;
+   
+		LDRH          r0,[r2,#0x00];
+		ADDS          r0,r0,#1;
+		STRH          r0,[r2,#0x00];
+    
+		AND           r0,r1,#0xFC;
+		CBZ           r0,l11;
+   
+		AND           r0,r1,#0x02;
+		CBNZ          r0,l11;
+    
+		LDRH          r5,[r2,#0x02];
+		ADDS          r5,r5,#1;
+		STRH          r5,[r2,#0x02];
+   
+		UBFX          r0,r1,#2,#3;
+    
+		UBFX          r4,r1,#5,#2;
+   
+		SUBS          r5,r0,#1;
+		ADDS          r6,r2,#6;
+		ADD           r5,r6,r5,LSL #2;
+		LDRB          r5,[r5,r4];
+		ADDS          r5,r5,#1;
+		UXTB          r7,r5;
+		SUBS          r5,r0,#1;
+		ADD           r5,r6,r5,LSL #2;
+		STRB          r7,[r5,r4];
+    
+		ADDS          r3,r3,#1;
+    
+		SUBS          r5,r0,#1;
+		ADD           r5,r6,r5,LSL #2;
+		LDRB          r5,[r5,r4];
+		CMP           r5,r0;
+		BNE           l10;
+
+		ADD           r3,r3,r0;
+
+l10     NOP           ;
+    
+l11     LDRH          r0,[r2,#0x04];
+		ADD           r0,r0,r3;
+		STRH          r0,[r2,#0x04];
+    
+		MOV           r0,r3;
+    
+		POP           {r4-r7,pc};
+		MOV           r0,#0x3000000;
+		VMSR          FPSCR, r0;
+		BX            lr;
+		MOVS          r0,r0;
   ENDFUNC
   
 
@@ -97,28 +105,30 @@ endloop1A	POP{r4-r5, pc}
 
 player_done_s FUNCTION	; char player_done_s(player_t * player);
 	; r0 *player
-	MOV R0, R3				; move player pointer to r3
-	MOV R0, #0x01				; won
-	MOV R1, #0x00				; i
-	MOV R2, #0x00				; j	
-loop1B	CMP R0, #0x04		; if i >= 4, end
-		BGE endloop1B		;
-
-loop2B	CMP R1, #0x04	;
-			BGE endloop2B	; if j >= 4, end
-			
-			ADDS r4, r3, #0x06		;
-			ADD r4, r4, r1, LSL #0x02	; calculate array offset
-			LDRB r5, [r4, r2]	; get value at calculated address
-			ADDS r4, r1, #0x01	; 
-			
-			CMP r5, r4		; if greater than, return
-			BGE endloop1B	;
-			ADDS R2, #0x01	; j++
-			B loop2B		; back to inner loop
-endloop2B 	ADDS R1, #0x01		; i++
-			B loop1B			; back to outter loop
-endloop1B	BX	LR					; return n
+		PUSH          {r4-r5,lr};
+		MOV           r3,r0;
+		MOVS          r0,#0x01;
+		MOVS          r1,#0x00;
+		B             l5;
+l1 		MOVS          r2,#0x00;
+		B             l6;
+l4 		ADDS          r4,r3,#6;
+		ADD           r4,r4,r1,LSL #2;
+		LDRB          r5,[r4,r2];
+		ADDS          r4,r1,#1;
+		CMP           r5,r4;
+		BGE           l2;
+		MOVS          r0,#0x00;
+		B             l3;
+l2   	ADDS          r2,r2,#1;
+l6 		RSB           r4,r1,#0x04;
+		CMP           r4,r2;
+		BGT           l4 ;
+l3      NOP           
+		ADDS          r1,r1,#1;
+l5      CMP           r1,#0x04;
+		BLT           l1;
+		POP           {r4-r5,pc};
   ENDFUNC
   
   END
